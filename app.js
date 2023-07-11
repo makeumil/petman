fe3c0255e6b2f1287336de0e43654a9f5047b5cc
makeumil <yesql@naver.com>
2023-07-11 20:22:20 +0900

    react 적용

--- app.js
+++ app.js
@@ -1,48 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var detailRouter = require('./routes/detail');
-var usersRouter = require('./routes/users');
-var noticeRouter = require('./routes/notice');
-
-var app = express();
-
-// stylesheet
-app.use(express.static('public'));
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/detail', detailRouter);
-app.use('/users', usersRouter);
-app.use('/notice', noticeRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.listen(3000, function(){
+    console.log('listening on 80');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(express.static(path.join(__dirname, 'petman-view/build')));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'petman-view/build/index.html'));
 });
-
-module.exports = app;
